112fd593c976ee9711b4502fba2f25cc36ce126d
Brian Light <brianlight2020@gmail.com>
2023-10-06 16:02:03 -0700

    Added functionality to data manager script
    Added ability to take name from title screen to gameplay
    
    Added method to save and load data to file

--- Assets/Scripts/UITitleManager.cs
+++ Assets/Scripts/UITitleManager.cs
@@ -2,16 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using TMPro;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
 
 public class UITitleManager : MonoBehaviour
 {
+    public TMP_InputField playerNameField;
 
     public void StartNew()
     {
+        DataManager.Instance.currentPlayerName = playerNameField.text;
         SceneManager.LoadScene(1);
     }
     public void Exit()
