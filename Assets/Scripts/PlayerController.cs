235af5a0b7b126bfdd078451123e8bcb237ec8ad
Brian Light <brianlight2020@gmail.com>
2023-10-06 23:29:18 -0700

    Add fire delay
    -added fire delay to limit rate of fire
    -made background slightly more interesting to shoot at.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,6 +12,8 @@ public class PlayerController : MonoBehaviour
     private bool isAlive = true;
     public float moveForce = 50;
     public float fireForce = 100;
+    public float fireDelay = 1;
+    private bool isReadyToFire = true;
 
     [SerializeField]
     private GameObject cannonBallPrefab;
@@ -37,35 +39,38 @@ public class PlayerController : MonoBehaviour
 
     void OnFire()
     {
-
-        Camera mainCamera = Camera.main;
-        Vector2 screenPosition = Mouse.current.position.ReadValue();
-        Vector3 mousePosition;
-
-        // Create a ray from the camera to the mouse position
-        Ray ray = mainCamera.ScreenPointToRay(screenPosition);
-
-        // Declare a variable to store the hit information
-        RaycastHit hit;
-
-        // Perform a raycast and check if it hits anything
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        if (isReadyToFire)
         {
-            // Get the point where the ray hits
-            mousePosition = hit.point;
+            Camera mainCamera = Camera.main;
+            Vector2 screenPosition = Mouse.current.position.ReadValue();
+            Vector3 mousePosition;
 
-            // Do something with the mouse position, for example print it to the console
-            Debug.Log("Mouse position in 3D space: " + mousePosition);
-            Vector3 fireVector = (mousePosition- transform.position).normalized;
-            GameObject newCannonBall = Instantiate(cannonBallPrefab, transform.position + Vector3.up, Quaternion.identity);
-            newCannonBall.GetComponent<Rigidbody>().AddForce(fireVector * fireForce);
+            // Create a ray from the camera to the mouse position
+            Ray ray = mainCamera.ScreenPointToRay(screenPosition);
+
+            // Declare a variable to store the hit information
+            RaycastHit hit;
+
+            // Perform a raycast and check if it hits anything
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                // Get the point where the ray hits
+                mousePosition = hit.point;
+
+                Vector3 fireVector = (mousePosition - transform.position).normalized;
+                GameObject newCannonBall = Instantiate(cannonBallPrefab, transform.position + Vector3.up, Quaternion.identity);
+                newCannonBall.GetComponent<Rigidbody>().AddForce(fireVector * fireForce);
+                isReadyToFire = false;
+                StartCoroutine(CannonBallTimer());
+            }
         }
-
-
-
-
     }
 
+    IEnumerator CannonBallTimer()
+    {
+        yield return new WaitForSeconds(fireDelay);
+        isReadyToFire = true;
+    }
     private void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
