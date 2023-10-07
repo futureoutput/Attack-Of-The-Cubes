3fde0f01a86c8fd909000372850e6d7774f62572
Brian Light <brianlight2020@gmail.com>
2023-10-06 23:14:04 -0700

    Added Player Fire Ability
    -created cannon ball prefab
    -overrode OnFire method in PlayerController to instantiate a cannon ball and launch it using a force towards wherever you click.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,6 +11,10 @@ public class PlayerController : MonoBehaviour
     private float movementY;
     private bool isAlive = true;
     public float moveForce = 50;
+    public float fireForce = 100;
+
+    [SerializeField]
+    private GameObject cannonBallPrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -29,6 +33,37 @@ public class PlayerController : MonoBehaviour
         Vector2 movementVector = movementValue.Get<Vector2>();
         movementX = movementVector.x;
         movementY = movementVector.y;
+    }
+
+    void OnFire()
+    {
+
+        Camera mainCamera = Camera.main;
+        Vector2 screenPosition = Mouse.current.position.ReadValue();
+        Vector3 mousePosition;
+
+        // Create a ray from the camera to the mouse position
+        Ray ray = mainCamera.ScreenPointToRay(screenPosition);
+
+        // Declare a variable to store the hit information
+        RaycastHit hit;
+
+        // Perform a raycast and check if it hits anything
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            // Get the point where the ray hits
+            mousePosition = hit.point;
+
+            // Do something with the mouse position, for example print it to the console
+            Debug.Log("Mouse position in 3D space: " + mousePosition);
+            Vector3 fireVector = (mousePosition- transform.position).normalized;
+            GameObject newCannonBall = Instantiate(cannonBallPrefab, transform.position + Vector3.up, Quaternion.identity);
+            newCannonBall.GetComponent<Rigidbody>().AddForce(fireVector * fireForce);
+        }
+
+
+
+
     }
 
     private void FixedUpdate()
