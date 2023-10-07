b88f257f84da5a19e7a03232e2ab9b0d6aec45af
Brian Light <brianlight2020@gmail.com>
2023-10-06 22:02:01 -0700

    Implemented basic enemy movement
    -added virtual method MoveEnemy to be overridden for more advanced enemies.
    -default behavior moves enemy toward player

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    private Rigidbody enemyRb;
+    private GameObject player;
+
+    public float moveTorque = 200;
+    public float pushForce = 100;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+
+    }
+    private void FixedUpdate()
+    {
+        MoveEnemy();
     }
 
-    // Update is called once per frame
-    void Update()
+    public virtual void MoveEnemy()
     {
-        
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+        Vector3 torqueDirection = -Vector3.Cross(lookDirection, Vector3.up).normalized;
+        enemyRb.AddTorque(Time.deltaTime * moveTorque * torqueDirection);
+        enemyRb.AddForce(Time.deltaTime * lookDirection * pushForce);
     }
+
 }
